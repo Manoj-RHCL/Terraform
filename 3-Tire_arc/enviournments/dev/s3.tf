ae3e458d02a98e5939505fe396929b683225896c
Manoj-RHCL <61321173+Manoj-RHCL@users.noreply.github.com>
2024-07-11 16:18:08 +0530

    Update s3.tf

--- 3-Tire_arc/enviournments/dev/s3.tf
+++ 3-Tire_arc/enviournments/dev/s3.tf
@@ -1,3 +1,3 @@
 https://github.com/Manoj-RHCL/Terraform/edit/main/3-Tire_arc/enviournments/dev/s3.tf
-AKIA2UC3FXTR6LKE33MN
-UNC0vwAPFTnOK7EdfB1M6znE9g5NOvFJttmWhaRE
+accesskey=AKIA2UC3FXTR6LKE33MN
+secukey=UNC0vwAPFTnOK7EdfB1M6znE9g5NOvFJttmWhaRE
